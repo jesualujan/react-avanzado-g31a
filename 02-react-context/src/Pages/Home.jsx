c403f5b8152cb5ed7a95e077237ea7ddafe2d3b9
jesualujan <lujan.oficial@gmail.com>
2024-05-16 20:47:58 -0600

    ✨ import de los componentes hijos

--- 02-react-context/src/Pages/Home.jsx
+++ 02-react-context/src/Pages/Home.jsx
@@ -1,8 +1,19 @@
-import React from 'react'
+import SongDetail from '../Components/SongDetail/SongDetail'
+import SongList from '../Components/SongList/SongList'
+import './home.css'
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <div className='home-container'>
+      <div className='left'>
+        <p>lado izquierdo</p>
+        <SongList />
+      </div>
+      <div className='right'>
+        <p>lado derecho</p>
+        <SongDetail />
+      </div>
+    </div>
   )
 }
 
