0e20558887fc1afea7c372cd73d00f5964c81f53
jesualujan <lujan.oficial@gmail.com>
2024-05-17 18:16:44 -0600

    ✨ creamos nuestros componentes

--- 02-react-context/src/Components/SongDetail/SongDetail.jsx
+++ 02-react-context/src/Components/SongDetail/SongDetail.jsx
@@ -1,7 +1,22 @@
+import { useSongContext } from '../../Hook/useSongContext'
 
 const SongDetail = () => {
+  const { selectedSong } = useSongContext()
   return (
-    <div>SongDetail</div>
+    <>
+      {
+      selectedSong.title
+        ? (
+          <div>
+            <img src={selectedSong.img_url} alt={selectedSong.title} />
+            <h4>{selectedSong.title}</h4>
+            <h5>{selectedSong.artist}</h5>
+            <h6>{selectedSong.year}</h6>
+          </div>
+          )
+        : <h1> Selecciona una canción </h1>
+    }
+    </>
   )
 }
 
