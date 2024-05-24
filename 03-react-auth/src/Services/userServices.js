1b318f53535864fa1d4a7db77adb573f0546ca45
jesualujan <lujan.oficial@gmail.com>
2024-05-23 23:05:27 -0600

    ✨ llamada a la api de Users

---
+++ 03-react-auth/src/Services/userServices.js
@@ -0,0 +1,22 @@
+// aquí hacemos la llamada a la api usando AXIOS 
+// le pasaremos un endpoint para que traiga la data
+
+//* 1) importar axios
+import axios from 'axios'
+
+//* 2) indicar la ruta o el endpoint 
+const BASE_URL = 'http://localhost:3000' // esto lo podemos cambiar si subimos el repo de github a render
+
+//* 3) ocupamos mandar información 
+// registrar un usuario
+const registerUserService = (data) => axios.post(`${BASE_URL}/register`, data)
+// login 
+const loginUserService = (data) => axios.post(`${BASE_URL}/login`, data)
+
+// información de un solo usuario -> jwt
+
+
+export {
+    registerUserService,
+    loginUserService
+}
