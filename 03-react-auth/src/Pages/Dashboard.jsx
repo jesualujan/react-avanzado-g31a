8adfacfa1c043a658d2add2acf1f620c4174f4f4
jesualujan <lujan.oficial@gmail.com>
2024-05-29 14:09:39 -0600

    ✨ perfil de usuario tipo: customer o admin

--- 03-react-auth/src/Pages/Dashboard.jsx
+++ 03-react-auth/src/Pages/Dashboard.jsx
@@ -1,7 +1,36 @@
+import { useState, useEffect } from "react"
+import { getUserService } from "@/Services/userServices"
 
 const Dashboard = () => {
+  const [userData, setUserData] = useState({})// vacío
+  const token = localStorage.getItem('token')
+
+  useEffect(() => {
+    const fetchUserData = async () => {
+      try {
+        const response = await getUserService(token)
+        if (response.status === 200) {
+          setUserData(response.data)
+        }
+      } catch (error) {
+        console.log('Ocurrio un error en Dashboard', error)
+      }
+    }
+    fetchUserData()
+  }, [token])
+
   return (
-    <div>Dashboard</div>
+    <>
+      <div>DASHBOARD</div> 
+      <h3> Bienvenido al Dashboard 😃</h3>
+      <h3> Bienvenido a tu perfil </h3>
+      <p> aca ustedes pueden agregar estilos de bootstrap, scss para crearle un card al usuario </p>
+      {userData?.first_name && <p>{userData.first_name}</p>}
+      {userData?.last_name && <p>{userData.last_name}</p>}
+      {userData?.gender && <p>{userData.gender}</p>}
+      {userData?.email && <p>{userData.email}</p>}
+      {userData?.role && <p>{userData.role}</p>}
+    </>
   )
 }
 
