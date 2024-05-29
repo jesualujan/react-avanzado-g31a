08a9e35502b51638d5d30dd7ed7b62dc01a090a6
jesualujan <lujan.oficial@gmail.com>
2024-05-28 19:24:19 -0600

    ✨ login funcionando

--- 03-react-auth/src/Pages/Login.jsx
+++ 03-react-auth/src/Pages/Login.jsx
@@ -1,10 +1,30 @@
+import { useForm } from 'react-hook-form'
+import { loginUserService } from '@/Services/userServices'
+import { useNavigate } from 'react-router-dom'
 import '@/styles/form.css'
 import logo from '@/assets/react.svg'
 const Login = () => {
+
+  const { register, handleSubmit, formState:{ errors } } = useForm();
+  const navigate = useNavigate()
+
+  const onSubmit = async (data) =>  {
+    //enviar mi formulario de signup
+    try {
+      const response = await loginUserService(data)
+      if (response.status === 200){
+        navigate('/')
+        console.log('Usuario autenticado exitosamente')
+        console.log(response.data.token)
+      }
+    }catch(error) {
+      console.log('Ocurrio un error en Login', error)
+    }
+  }
+
   return (
-  
     <main className='form-signin w-100 m-auto'>
-      <form>
+      <form onSubmit={handleSubmit(onSubmit)}>
         <img
           className='mb-4'
           src={logo}
@@ -19,7 +39,9 @@ const Login = () => {
             className='form-control'
             id='floatingInput'
             placeholder='name@example.com'
+            {...register('email')}
           />
+           <p>{errors.email?.message}</p>
           <label htmlFor='floatingInput'>Email address</label>
         </div>
         <div className='form-floating'>
@@ -28,14 +50,16 @@ const Login = () => {
             className='form-control'
             id='floatingPassword'
             placeholder='Password'
+            {...register('password')}
           />
+           <p>{errors.password?.message}</p>
           <label htmlFor='floatingPassword'>Password</label>
         </div>
 
         <button className='btn btn-primary w-100 py-2' type='submit'>
           Sign in
         </button>
-        <p className='mt-5 mb-3 text-body-secondary'>© 2017–2023</p>
+        <p className='mt-5 mb-3 text-body-secondary'>© 2017–2024</p>
       </form>
     </main>
   )
