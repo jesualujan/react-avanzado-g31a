1c1d7f962067f203cafcf3239df43f7e2fa40727
jesualujan <lujan.oficial@gmail.com>
2024-05-29 14:08:02 -0600

    ✨ importamos el contexto en app

--- 03-react-auth/src/App.jsx
+++ 03-react-auth/src/App.jsx
@@ -1,14 +1,17 @@
 import { BrowserRouter } from "react-router-dom";
 import RoutesIndex from "@/Routes/RoutesIndex";
 import Header from "@/Components/Header";
+import { AuthProvider } from "@/Context/AuthContext";
 
 function App() {
   return (
     <>
-    <BrowserRouter>
+    <AuthProvider>
+     <BrowserRouter>
        <Header />
        <RoutesIndex/>
     </BrowserRouter>
+    </AuthProvider>
     </>
   );
 }
