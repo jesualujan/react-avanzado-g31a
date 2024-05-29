547b8fcb44560d47fba766aaa52b09a2a135a077
jesualujan <lujan.oficial@gmail.com>
2024-05-28 20:02:18 -0600

    ✨ creamos nuestro consumidor del contexto

---
+++ 03-react-auth/src/Hook/useAuthContext.jsx
@@ -0,0 +1,13 @@
+// consumidor del contexto 
+import { useContext } from "react";
+import { AuthContext } from "@/Context/AuthContext";
+
+export const useAuthContext = () => {
+      // quiero usar mi contexto y esto lo guardo en context
+  const context = useContext(AuthContext)
+  // si no se crea el contexto mando un error, si es undefined (vacío) lanzo el error
+  if (!context) {
+    throw Error('useAuthContext debe ser usado dentro de AuthProvider')
+  }
+  return context
+}
