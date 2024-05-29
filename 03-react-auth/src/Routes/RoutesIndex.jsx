22dde2429b88f296eeceb354e31b857b52fc7ae9
jesualujan <lujan.oficial@gmail.com>
2024-05-28 20:41:52 -0600

    ✨ rutas protegidas

--- 03-react-auth/src/Routes/RoutesIndex.jsx
+++ 03-react-auth/src/Routes/RoutesIndex.jsx
@@ -1,15 +1,31 @@
 // Implementar react-router-dom
 import { Routes, Route } from 'react-router-dom'
 import { Home, Dashboard, Login, Secret, Signup } from '@/Pages'
+import { useAuthContext } from '@/Hook/useAuthContext'
 
 const RoutesIndex = () => {
+  const { isAuth } = useAuthContext()
   return (
     <Routes>
         <Route path='/' element={<Home />} />
-        <Route path='/dashboard' element={<Dashboard />} />
+        <Route 
+        path='/dashboard' 
+        element={
+        isAuth
+        ? <Dashboard />
+        : <Login /> 
+        } 
+        />
         <Route path='/login' element={<Login/>} />
-        <Route path='/secret' element={<Secret />} />
-        <Route path='/signup' element={<Signup />} />
+        <Route 
+        path='/secret' 
+        element={
+        isAuth
+        ? <Secret />
+       : <Login />
+       } 
+       />
+      <Route path='/signup' element={<Signup />} />
     </Routes>
   )
 }
