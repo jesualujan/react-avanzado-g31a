cd6e8e0a7c7aface135fa81d65ce2e478a930327
jesualujan <lujan.oficial@gmail.com>
2024-05-28 19:24:03 -0600

    ✨ signup funcionando

--- 03-react-auth/src/Pages/Signup.jsx
+++ 03-react-auth/src/Pages/Signup.jsx
@@ -1,10 +1,31 @@
+import { useForm } from 'react-hook-form'
+import { registerUserService } from '@/Services/userServices'
+import { useNavigate } from 'react-router-dom'
 import '@/styles/form.css'
 import logo from '@/assets/react.svg'
 
 const Signup = () => {
+
+  const { register, handleSubmit, formState:{ errors } } = useForm();
+  const navigate = useNavigate()
+
+  const onSubmit = async (data) =>  {
+    //enviar mi formulario de signup
+    try {
+      const response = await registerUserService(data)
+      if (response.status === 201){
+        navigate('/login')
+        console.log('Usuario creado exitosamente')
+      }
+    }catch(error) {
+      console.log('Ocurrio un error en Signup', error)
+    }
+  }
+
+
   return (
-    <main className='form-signin w-100 m-auto'>
-      <form>
+    <main className='form-signin w-100'>
+      <form onSubmit={handleSubmit(onSubmit)}>
         <img className='mb-4' src={logo} alt='' width='72' height='57' />
         <h1 className='h3 mb-3 fw-normal'>Please sign up</h1>
 
@@ -14,10 +35,10 @@ const Signup = () => {
             className='form-control'
             id='first_name'
             name='first_name'
-            value=''
-            onChange={() => {}}
             placeholder='John'
+            {...register('first_name')}
           />
+          <p>{errors.first_name?.message}</p>
           <label htmlFor='first_name'>First Name</label>
         </div>
 
@@ -27,10 +48,10 @@ const Signup = () => {
             className='form-control'
             id='last_name'
             name='last_name'
-            value=''
-            onChange={() => {}}
             placeholder='Doe'
+            {...register('last_name')}
           />
+          <p>{errors.last_name?.message}</p>
           <label htmlFor='last_name'>Last Name</label>
         </div>
 
@@ -39,9 +60,9 @@ const Signup = () => {
             className='form-select'
             id='gender'
             name='gender'
-            value=''
-            onChange={() => {}}
+            {...register('gender')}
           >
+            <p>{errors.gender?.message}</p>
             <option value=''>Choose...</option>
             <option value='M'>Male</option>
             <option value='F'>Female</option>
@@ -55,10 +76,10 @@ const Signup = () => {
             className='form-control'
             id='email'
             name='email'
-            value=''
-            onChange={() => {}}
             placeholder='name@example.com'
+            {...register('email')}
           />
+          <p>{errors.email?.message}</p>
           <label htmlFor='email'>Email address</label>
         </div>
 
@@ -68,16 +89,16 @@ const Signup = () => {
             className='form-control'
             id='password'
             name='password'
-            value=''
-            onChange={() => {}}
             placeholder='Password'
+            {...register('password')}
           />
+          <p>{errors.password?.message}</p>
           <label htmlFor='password'>Password</label>
         </div>
 
         <button className='w-100 btn btn-lg btn-primary' type='submit'>Sign up</button>
-        <p className='mt-5 mb-3 text-muted'>© 2017–2022</p>
-      </form>
+        <p className='mt-5 mb-3 text-body-secondary'>© 2017–2024</p>
+        </form>
     </main>
   )
 }
